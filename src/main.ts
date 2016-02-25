21a29853c4ca9c19fed1fd93df5481aa7524e852
Aldrin Leal <aldrin@leal.eng.br>
2016-02-25 03:26:22 -0500

    First Version

---
+++ src/main.ts
@@ -0,0 +1,75 @@
+///<reference path="../typings/browser.d.ts"/>
+
+class Dependency {
+    groupId:string;
+
+    artifactId:string;
+
+    packaging:string;
+
+    version:string;
+
+    scope:string;
+
+    constructor(groupId?:string, artifactId?:string, packaging?:string, version?:string, scope?:string) {
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+        this.packaging = packaging;
+        this.version = version;
+
+        if (scope) {
+            this.scope = scope;
+        } else {
+            this.scope = 'compile';
+        }
+    }
+
+    asDependency():string {
+        var result = `<dependency>
+  <groupId>${this.groupId}</groupId>
+  <artifactId>${this.artifactId}</artifactId>
+  <version>${this.version}</version>
+`
+
+        if ('jar' != this.packaging) {
+            result += `<packaging>${this.packaging}</packaging>`
+        }
+
+        if ('compile' !== this.scope) {
+            result += `<scope>${this.packaging}</scope>`
+        }
+
+        result += `</dependency>
+`
+
+        return result;
+    }
+}
+
+$(function () {
+    $('#do').on('click',  (e: JQueryEventObject) => {
+        e.preventDefault();
+
+        var t = $("#text").text();
+
+        var dependencies = t.match(/[^\s]+:[^\s]+:[^\s]+:[^\s]+:[^\s]+(:[^\s]+)?/g).map((x:string) => {
+            //console.log('x: ', x);
+
+            var elts = x.split(/:/);
+
+            var d = new Dependency();
+
+            d.constructor.apply(d, elts);
+
+            return d;
+        }).map((d) => d.asDependency());
+
+        console.log('dependencies: ', JSON.stringify(dependencies, null, 2));
+
+        var htmlContent = dependencies.join("\n");
+
+        $("#result").append("<pre>")
+        $("#result pre").text(htmlContent);
+        //.text(htmlContent);
+    })
+});
